d69882936db643216c6d65f951a4e72cdb953c8c
William Cavalcante <williambreno@mat.ci.ufpb.br>
2019-08-28 21:29:08 -0300

    Test instances and input files

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,120 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <fstream>
 
-void _countingSort(const std::vector<uint32_t>& in, std::vector<uint32_t>& out,
-                  uint32_t k) {
+void _countingSort(const std::vector<int32_t>& in, std::vector<int32_t>& out, int32_t k) {
   out.resize(in.size());
-  std::vector<uint32_t> count(k + 1, 0);
+  std::vector<int32_t> count(k + 1, 0);
 
-  for (uint32_t i = 0; i < in.size(); ++i) count[in[i]] += 1;
+  for (int32_t i = 0; i < in.size(); ++i) count[in[i]] += 1;
 
-  for (uint32_t i = 1; i <= k; ++i) count[i] = count[i] + count[i - 1];
+  for (int32_t i = 1; i <= k; ++i) count[i] = count[i] + count[i - 1];
 
-  for (uint32_t i = in.size(); i >= 1; --i) {
+  for (int32_t i = in.size(); i >= 1; --i) {
     out[count[in[i - 1]] - 1] = in[i - 1];
     count[in[i - 1]] = count[in[i - 1]] - 1;
   }
 }
 
-void countingSort(std::vector<uint32_t>& v) {
-  std::vector<uint32_t> out(v.size());
+void countingSort(std::vector<int32_t>& v) {
+  int32_t min = *std::min_element(v.begin(), v.end());
+  bool neg = min < 0;
+
+  if (neg)
+    for (auto& i : v) i -= min;
+
+  std::vector<int32_t> out(v.size());
   _countingSort(v, out, *std::max_element(v.begin(), v.end()));
   v = out;
-}
 
+  if (neg)
+    for (auto& i : v) i += min;
+}
 
 #define RADIX_DIGIT(n, exp) (((n)/(exp))%10)
 
-void _countingSortRadix(const std::vector<uint32_t>& in, std::vector<uint32_t>& out,
-                  uint32_t k, int exp) {
+void _countingSortRadix(const std::vector<int32_t>& in, std::vector<int32_t>& out, int exp) {
   out.resize(in.size());
-  std::vector<uint32_t> count(k + 1, 0);
+  constexpr int32_t k = 9;
+  std::vector<int32_t> count(k + 1, 0);
 
-  for (uint32_t i = 0; i < in.size(); ++i) count[RADIX_DIGIT(in[i], exp)] += 1;
+  for (int32_t i = 0; i < in.size(); ++i) count[RADIX_DIGIT(in[i], exp)] += 1;
 
-  for (uint32_t i = 1; i <= k; ++i) count[i] = count[i] + count[i - 1];
+  for (int32_t i = 1; i <= k; ++i) count[i] = count[i] + count[i - 1];
 
-  for (uint32_t i = in.size(); i >= 1; --i) {
-    uint32_t a = RADIX_DIGIT(in[i - 1], exp);
+  for (int32_t i = in.size(); i >= 1; --i) {
+    int32_t a = RADIX_DIGIT(in[i - 1], exp);
 
     out[count[a] - 1] = in[i -1];
     --count[a];
   }
 }
 
-void _radixSort(const std::vector<uint32_t>& in, std::vector<uint32_t>& out, uint32_t max) {
-  std::vector<uint32_t> v = in;
+void _radixSort(const std::vector<int32_t>& in, std::vector<int32_t>& out, int32_t max) {
+  std::vector<int32_t> v = in;
 
-  for (uint32_t exp = 1; 1000/exp > 0; exp *= 10) {
-    _countingSortRadix(v, out, 9, exp);
+  for (int32_t exp = 1; max/exp > 0; exp *= 10) {
+    _countingSortRadix(v, out, exp);
     v = out;
   }
 }
 
-void radixSort(std::vector<uint32_t>& v) {
-  std::vector<uint32_t> out(v.size());
-  uint32_t max = *std::max_element(v.begin(), v.end());
+void radixSort(std::vector<int32_t>& v) {
+  int32_t min = *std::min_element(v.begin(), v.end());
+  bool neg = min < 0;
+
+  if (neg)
+    for (auto& i : v) i -= min;
+
+  std::vector<int32_t> out(v.size());
+  int32_t max = *std::max_element(v.begin(), v.end());
 
   _radixSort(v, out, max);
 
   v = out;
+
+  if (neg)
+    for (auto& i : v) i += min;
 }
 
-int main() {
-  std::vector<uint32_t> v = {231, 9, 703, 611, 343, 772, 895, 666, 100, 187, 121};
-  std::vector<uint32_t> out(v.size());
+int main(int argc, char** argv) {
+  if (argc != 3) {
+    std::cout << "usage: bin/sort <option> <test_file_path>\n";
+    std::cout << "\t options: c (counting sort), r (radix sort)\n";
+    std::exit(EXIT_SUCCESS);
+  }
 
-  out = v;
+  std::string file_path(argv[2]);
+  std::ifstream fs(file_path, std::ios::binary);
 
-  radixSort(out);
+  if (!fs.is_open()) {
+    std::cout << "ERROR: Failed to open file: " + file_path << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
 
-  for (auto& i : out) std::cout << i << "\n";
+  int32_t num = 0;
+  fs >> num;
+
+  std::vector<int32_t> in(num, 0);
+  for (auto& i : in) fs >> i;
+  fs.close();
+
+  char select = argv[1][0];
+  if (select == 'c') {
+    countingSort(in);
+  } else if (select == 'r') {
+    radixSort(in);
+  } else {
+    std::cout << "\"" << select << "\" - is not a valid option\n";
+    std::exit(EXIT_FAILURE);
+  }
+
+
+  std::ofstream ofs("output.txt", std::ios::binary);
+  for (auto& i : in) ofs << (int32_t)i << "\n";
+
+  ofs.close();
 
   std::exit(EXIT_SUCCESS);
 }
